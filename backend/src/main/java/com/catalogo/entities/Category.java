a61705dfe0480ebf67b74b092e5382ebd7ecd342
Erick Almeida <erickprofissional@gmail.com>
2021-01-20 00:56:14 -0300

    Categoria concluida

--- backend/src/main/java/com/catalogo/entities/Category.java
+++ backend/src/main/java/com/catalogo/entities/Category.java
@@ -1,6 +1,8 @@
 package com.catalogo.entities;
 
-public class Category {
+import java.io.Serializable;
+
+public class Category implements Serializable {
 
     private Long id;
     private String name;
