8300738ce63605b794ce076361a7bc147014e202
Erick Almeida <erickprofissional@gmail.com>
2021-01-20 00:15:35 -0300

    Projeto criado

---
+++ backend/src/test/java/com/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
