8e3c838696844cef2aaac5ee7c8f855b36edf72e
Erick Almeida <erickprofissional@gmail.com>
2021-01-20 23:53:31 -0300

    populando H2 seeding

--- backend/src/main/java/com/catalogo/services/CategoryService.java
+++ backend/src/main/java/com/catalogo/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.catalogo.entities.Category;
 import com.catalogo.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
 
         return categoryRepository.findAll();
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category (name) VALUES ('Livros');
+INSERT INTO tb_category (name) VALUES ('Eletronicos');
+INSERT INTO tb_category (name) VALUES ('Computadores');
