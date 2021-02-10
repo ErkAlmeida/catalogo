31565fc5c32e88671c7dccb42b2e5abbc8013f2b
Erick Almeida <erickprofissional@gmail.com>
2021-02-09 23:04:02 -0300

    iniciando frontend

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from  'react';
+import Alert from './Alert';
+
+
+const App = () =>{
+    return(
+        <div className= "container mt-5">
+            <Alert text ="Teu cu"/>
+            <Alert text ="Erick Almeida"/>
+        </ div>
+    );
+}
+
+export default App;
