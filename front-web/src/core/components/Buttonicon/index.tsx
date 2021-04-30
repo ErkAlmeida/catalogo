eeeac0de1813b216b9696c54fd05b5a463f32c9b
Erick Almeida <erickprofissional@gmail.com>
2021-04-29 23:29:52 -0300

    Fluxo e pagina home concluido

---
+++ front-web/src/core/components/Buttonicon/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./styles.scss";
+import { ReactComponent as Arronwicon } from "../../assets/images/arrow.svg"
+
+
+type Props = {
+    text: string;
+}
+
+
+const Buttonicon = ({text}: Props) => (
+
+    <div className="d-flex">
+        <button className="btn btn-primary btn-icon">
+            <h5> {text} </h5>
+        </button>
+        <div className="btn-icon-content">
+            <Arronwicon />
+        </div>
+    </div>
+);
+
+
+export default Buttonicon;
--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import{ReactComponent as MainImage} from '../../core/assets/images/main-image.svg';
+import Buttonicon from '../../core/components/Buttonicon';
+import {Link} from 'react-router-dom';
+import "./styles.scss";
 
 const Home = () =>(
-    <div>
-        <h1>Home</h1>
+    <div className="home-container">
+        <div className="row home-content">
+          <div className="col-6">
+            <h1 className="text-title">Conheça o melhor <br /> catálogo de produtos</h1>
+            <p className="text-subtitle">
+                Ajudaremos você a encontrar os melhores <br /> produtos disponíveis no mercado.
+            </p>
+            <Link to="/catalogo">
+            <Buttonicon text ="inicie agora a sua busca" />
+            </Link>
+          </div>
+          <div className="col-6">
+              <MainImage className ="main-image" />
+          </div>
+        </div>
     </div>
 );
 
