4c6421bfc0cb164262a988294c5a157ecaa4b69d
Almeida <erick almeida santos>
2021-12-17 18:45:53 -0300

    Concluido o Crud do usuario

--- backend/src/main/java/com/catalogo/entities/User.java
+++ backend/src/main/java/com/catalogo/entities/User.java
@@ -17,6 +17,8 @@ public class User implements Serializable {
     private Long id;
     private String firstName;
     private String lastName;
+
+    @Column(unique = true)
     private String email;
     private String password;
 
---
+++ backend/src/main/java/com/catalogo/resource/UserResource.java
@@ -0,0 +1,61 @@
+package com.catalogo.resource;
+
+import com.catalogo.dto.UserDTO;
+import com.catalogo.dto.UserInsertDTO;
+import com.catalogo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "firstName") String orderBy
+    ){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage,Sort.Direction.valueOf(direction), orderBy);
+        Page<UserDTO> list = service.findAll(pageRequest);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value= "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        UserDTO dto = service.findBYId(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto){
+        UserDTO newDTO = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}")
+                .buildAndExpand(newDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(newDTO);
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
