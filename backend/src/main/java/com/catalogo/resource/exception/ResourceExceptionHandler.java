1042bbce66046fcc73c6d8d957202646fce995cf
Almeida <erick almeida santos>
2022-01-13 23:01:46 -0300

    implementação co ValidationError

--- backend/src/main/java/com/catalogo/resource/exception/ResourceExceptionHandler.java
+++ backend/src/main/java/com/catalogo/resource/exception/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.catalogo.services.exception.DatabaseException;
 import com.catalogo.services.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -29,6 +31,7 @@ public class ResourceExceptionHandler {
 
         return ResponseEntity.status(statusNot).body(err);
     }
+
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request){
 
@@ -40,6 +43,23 @@ public class ResourceExceptionHandler {
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
 
+        return ResponseEntity.status(statusBad).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+
+        ValidationError err = new ValidationError();
+
+        err.setTimestamp(Instant.now());
+        err.setStatus(statusBad.value());
+        err.setError("Validation exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        for(FieldError f: e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(statusBad).body(err);
     }
 }
--- backend/src/main/java/com/catalogo/resource/exception/StandardError.java
+++ backend/src/main/java/com/catalogo/resource/exception/StandardError.java
@@ -1,8 +1,11 @@
 package com.catalogo.resource.exception;
 
+import java.io.Serializable;
 import java.time.Instant;
 
-public class StandardError {
+public class StandardError implements Serializable {
+
+    private static final long serialVersionUID =1L;
 
     private Instant timestamp;
     private Integer status;
