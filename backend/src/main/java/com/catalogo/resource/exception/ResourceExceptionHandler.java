3e891dfa527f06aaa956bc7d7d42ae3a3df29c90
Almeida <erick almeida santos>
2022-04-20 00:27:00 -0300

    correção para retorno de http code 422

--- backend/src/main/java/com/catalogo/resource/exception/ResourceExceptionHandler.java
+++ backend/src/main/java/com/catalogo/resource/exception/ResourceExceptionHandler.java
@@ -6,53 +6,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class ResourceExceptionHandler {
-
-    private HttpStatus statusBad = HttpStatus.BAD_REQUEST;
-    private HttpStatus statusNot = HttpStatus.NOT_FOUND;
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
-
+        HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError();
 
         err.setTimestamp(Instant.now());
-        err.setStatus(statusNot.value());
+        err.setStatus(status.value());
         err.setError("Resource Not Found");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
 
-        return ResponseEntity.status(statusNot).body(err);
+        return ResponseEntity.status(status).body(err);
     }
 
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request){
-
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError();
 
         err.setTimestamp(Instant.now());
-        err.setStatus(statusBad.value());
+        err.setStatus(status.value());
         err.setError("Resource Not Found");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
 
-        return ResponseEntity.status(statusBad).body(err);
+        return ResponseEntity.status(status).body(err);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
-
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
         ValidationError err = new ValidationError();
 
         err.setTimestamp(Instant.now());
-        err.setStatus(statusBad.value());
+        err.setStatus(status.value());
         err.setError("Validation exception");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
@@ -60,6 +57,6 @@ public class ResourceExceptionHandler {
         for(FieldError f: e.getBindingResult().getFieldErrors()) {
             err.addError(f.getField(), f.getDefaultMessage());
         }
-        return ResponseEntity.status(statusBad).body(err);
+        return ResponseEntity.status(status).body(err);
     }
 }
