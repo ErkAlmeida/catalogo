5737c0a4104ffbc6406d8b77fc492aeed464e3dc
Almeida <erick almeida santos>
2022-08-03 21:29:49 -0300

    configuração do swagger

--- backend/src/main/java/com/catalogo/config/WebSecurityConfig.java
+++ backend/src/main/java/com/catalogo/config/WebSecurityConfig.java
@@ -27,6 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/actuator/**");
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+                "/configuration/**", "/swagger-ui.html", "/webjars/**");
     }
 
     @Override
