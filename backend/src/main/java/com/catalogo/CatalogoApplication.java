d7be9ceb5f4d52a3b816bee2154ddcde7c49d553
Almeida <erick almeida santos>
2022-08-11 20:28:22 -0300

    configuração do S3 e aws

--- backend/src/main/java/com/catalogo/CatalogoApplication.java
+++ backend/src/main/java/com/catalogo/CatalogoApplication.java
@@ -1,13 +1,22 @@
 package com.catalogo;
 
+import com.catalogo.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatalogoApplication {
+public class CatalogoApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception{
+		s3Service.uploadFile("/home/almeida/Downloads/img-profile.svg");
 	}
 
 }
