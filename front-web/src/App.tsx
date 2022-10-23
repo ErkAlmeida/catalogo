4f6756c6703ad4d709803d32746962f7ef24453b
Almeida <erick almeida santos>
2022-10-23 00:29:58 -0300

    config inicial

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './core/assets/styles/custom.scss';
-import './app.scss';
-import Routes from './Routes';
+import './assets/styles/custom.scss'
+import './App.scss';
+
 
 const App = () =>{
     return(
-        <Routes />
+        <h1>Hello</h1>
     ); 
 }
 
--- front-web/src/core/utils/list.ts
+++
@@ -1,5 +0,0 @@
-export const generateList = (amount: number) => {
-
-    return Array.from(Array(amount).keys());
-
-} 
