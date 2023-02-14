0969e0577cbc3fa722337e4fd096deb6116581a6
Almeida <erick almeida santos>
2023-02-13 22:33:31 -0300

    mapeando rotas

---
+++ front-web/src/components/ProductPrice/index.tsx
@@ -0,0 +1,12 @@
+import './styles.scss'
+
+const ProductPrice = () => {
+    return (
+        <div className='produt-price-container'>
+            <span>R$</span>
+            <h3>2345.55</h3>
+        </div>
+    );
+}
+
+export default ProductPrice;
---
+++ front-web/src/pages/Admin/index.tsx
@@ -0,0 +1,5 @@
+const Admin = () => {
+  return <h1>Pagina Admin</h1>;
+};
+
+export default Admin;
